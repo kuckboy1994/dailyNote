63778da3317e89f406abe1f059ad44ce9da8e0fb
kuckboy1994 <shanchao@qq.com>
2017-06-20 15:00:40 +0800

    jq插件写法

---
+++ jQuery/component_demo/demo1.js
@@ -0,0 +1,22 @@
+/* @charset "UTF-8" */
+/*
+ * @author 山超<shanchao@qq.com>
+ * @create 2017-06-20
+ */
+(function ($) {
+	//默认参数
+	var defaluts = {
+
+	};
+
+	$.fn.extend({
+		
+		xxx: function (options) {
+			var _self = this;
+			var options = $.extend({}, defaluts, options);
+
+		}
+	});
+	
+})(jQuery);
+
